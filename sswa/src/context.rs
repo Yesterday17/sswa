0467c48dfd36ec4e498a8d8f74d14a9905ec070c
Yesterday17 <mmf@mmf.moe>
2022-03-12 11:53:39 +0800

    修复 ss_ 前缀

--- sswa/src/context.rs
+++ sswa/src/context.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
+use std::fmt::format;
 use lazy_static::lazy_static;
 use parking_lot::RwLock;
+use serde_json::Value;
 
 lazy_static! {
     pub static ref CONTEXT: Context = Context::new();
 }
 
-pub struct Context(pub(crate) RwLock<HashMap<String, String>>);
+pub struct Context(pub(crate) RwLock<HashMap<String, Value>>);
 
 impl Context {
     pub fn new() -> Self {
@@ -14,9 +16,9 @@ impl Context {
     }
 
     pub fn insert_sys<S>(&self, mut key: String, value: S)
-        where S: Into<String> {
+        where S: Into<Value> {
         let value = value.into();
-        self.insert(key.clone(), value.clone());
+        self.insert(format!("${key}"), value.clone());
 
         // 兼容考虑的 ss_{var}
         key.insert_str(0, "ss_");
@@ -24,16 +26,8 @@ impl Context {
     }
 
     pub fn insert<S>(&self, key: String, value: S)
-        where S: Into<String> {
+        where S: Into<Value> {
         self.0.write().insert(key, value.into());
-    }
-
-    pub fn get(&self, key: &str) -> anyhow::Result<String> {
-        let me = self.0.read();
-        Ok(me.get(key)
-            .map(|value| value.into())
-            .ok_or_else(|| anyhow::anyhow!("{} not found", key))?
-        )
     }
 
     pub fn contains_key(&self, key: &str) -> bool {
